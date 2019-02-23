524423136d101e3f31efbc0efa97d2fdbe989bf0
Observed Observer <MasterHaoChen@icloud.com>
2019-02-23 23:35:35 +0800

    add auto-py-script

--- check.js
+++ check.js
@@ -1,17 +1,47 @@
 const fs = require('fs')
+const args = global.process.argv.slice(2)
+const lastRecord = args[0].split('=')[1]
+const currentRecord = args[1].split('=')[1]
+const data1 = JSON.parse(fs.readFileSync(currentRecord).toString())
 
-const data1 = JSON.parse(fs.readFileSync('[fans-data]2019-02-23 22:14:39.json').toString())
-console.log(data1.length)
 let map1 = new Map()
 data1.forEach(item => {
   map1.set(item.id, item)
 })
 
-const data2 =  JSON.parse(fs.readFileSync('a').toString())
-console.log(data2.length)
+const data2 =  JSON.parse(fs.readFileSync(lastRecord).toString())
 
 data2.forEach(item => {
   if (!map1.has(item.id)) {
-    console.log(item)
+    const {
+      url_token,
+      is_followed,
+      type,
+      url,
+      name,
+      articles_count,
+      headline,
+      answer_count,
+      is_org,
+      follower_count,
+      id,
+      is_following,
+      gender
+    } = item
+    console.table({
+      url_token,
+      is_followed,
+      type,
+      url,
+      name,
+      articles_count,
+      headline,
+      answer_count,
+      is_org,
+      follower_count,
+      id,
+      is_following,
+      gender
+    })
   }
 })
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from request import getData
-import time, json
+import time, json, os
 if __name__ == '__main__':
-  t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
+  t = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
   data = getData()
-  with open('[fans-data]' + t + '.json', 'w+') as f:
+  filepath = './data/[fans-data]' + t + '.json'
+  with open(filepath, 'w+') as f:
     f.write(json.dumps(data))
+  with open('./record.json', 'r') as f:
+    records = json.loads(f.read())
+    records.append(filepath)
+    with open('./record.json', 'w') as w:
+      w.write(json.dumps(records))
+    compare = records[-2:]
+    if len(compare) == 2:
+      os.system('node check.js -last=' + compare[0] + ' -current=' + compare[1])
+    else:
+      print('Record saved. No last time record provided.\n')
